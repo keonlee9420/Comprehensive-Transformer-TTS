be3adc092d7179035bec1e433669def4fbe35922
keonlee9420 <keonlee9420@gmail.com>
2021-11-28 10:11:46 +0900

    delightfultts_up_no_prosody_loss

--- model/modules.py
+++ model/modules.py
@@ -819,10 +819,11 @@ class VarianceAdaptor(nn.Module):
                 phoneme_prosody_embeddings, phoneme_prosody_attn = self.phoneme_prosody_encoder(x, src_len, src_mask, mel, mel_len, mel_mask)
 
                 # utterance_prosody_vectors = self.utterance_prosody_predictor(x)
+                x = x + self.utterance_prosody_prj(utterance_prosody_embeddings)
                 # x = x + (self.utterance_prosody_prj(utterance_prosody_embeddings) if self.training else
                 #         self.utterance_prosody_prj(utterance_prosody_vectors))
-                phoneme_prosody_vectors = self.phoneme_prosody_predictor(x)
-                x = x + self.phoneme_prosody_prj(phoneme_prosody_embeddings)
+                # phoneme_prosody_vectors = self.phoneme_prosody_predictor(x)
+                # x = x + self.phoneme_prosody_prj(phoneme_prosody_embeddings)
                 # x = x + (self.phoneme_prosody_prj(phoneme_prosody_embeddings) if self.training else
                 #         self.phoneme_prosody_prj(phoneme_prosody_vectors))
                 prosody_info = (
